874ce85ce688e53422d594c2fd5b0147919588c2
Raissa <37599780+ThiAmaral@users.noreply.github.com>
2023-01-31 21:18:38 -0300

    CriacaoServer e AjusteValentao

--- src/algoritimovalentao/AlgoritimoValentao.java
+++ src/algoritimovalentao/AlgoritimoValentao.java
@@ -10,115 +10,138 @@ import java.util.Scanner;
  * @author Thiago
  */
 public class AlgoritimoValentao {
-
-    /**
-     * @param args the command line arguments
-     */
-    static boolean[] state = new boolean[5];
-    int coordinator;
-
-    public AlgoritimoValentao() {
-    }
-
-    public static void up(int up) {
-        if (state[up - 1]) {
-            System.out.println("process" + up + "is already up");
-        } else {
-            state[up - 1] = true;
-            System.out.println("process " + up + "held election");
-
-            int i;
-            for(i = up; i < 5; ++i) {
-                System.out.println("election message sent from process" + up + "to process" + (i + 1));
-            }
-
-            for(i = up + 1; i <= 5; ++i) {
-                if (state[i - 1]) {
-                    System.out.println("alive message send from process" + i + "to process" + up);
-                    break;
-                }
-            }
-        }
-
-    }
-
-    public static void down(int down) {
-        if (!state[down - 1]) {
-            System.out.println("process " + down + "is already dowm.");
-        } else {
-            state[down - 1] = false;
-        }
-
-    }
-
-    public static void mess(int mess) {
-        if (state[mess - 1]) {
-            if (state[4]) {
-                System.out.println("0K");
-            } else if (!state[4]) {
-                System.out.println("process" + mess + "election");
-
-                int i;
-                for(i = mess; i < 5; ++i) {
-                    System.out.println("election send from process" + mess + "to process " + (i + 1));
-                }
-
-                for(i = 5; i >= mess; --i) {
-                    if (state[i - 1]) {
-                        System.out.println("Coordinator message send from process" + i + "to all");
-                        break;
-                    }
-                }
-            }
-        } else {
-            System.out.println("Prccess" + mess + "is down");
-        }
-
-    }
-
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-
-        int up;
-        for(up = 0; up < 5; ++up) {
-            state[up] = true;
-        }
-
-        System.out.println("5 active process are:");
-        System.out.println("Process up  = p1 p2 p3 p4 p5");
-        System.out.println("Process 5 is coordinator");
-
-        int choice;
-        do {
-            System.out.println(".........");
-            System.out.println("1 up a process.");
-            System.out.println("2.down a process");
-            System.out.println("3 send a message");
-            System.out.println("4.Exit");
-            choice = sc.nextInt();
-            switch (choice) {
-                case 1:
-                    System.out.println("bring proces up");
-                    up = sc.nextInt();
-                    if (up == 5) {
-                        System.out.println("process 5 is co-ordinator");
-                        state[4] = true;
-                    } else {
-                        up(up);
-                    }
-                    break;
-                case 2:
-                    System.out.println("bring down any process.");
-                    up = sc.nextInt();
-                    down(up);
-                    break;
-                case 3:
-                    System.out.println("which process will send message");
-                    up = sc.nextInt();
-                    mess(up);
-            }
-        } while(choice != 4);
-
-    }
+    
+    //criar um novo socket, mensagem, processo/prioridade > eleito
+        // initialize variables and array  
+    class Process{  
+    // declare variables  
+    public int id;  
+    public String status;  
+      
+    // initialize variables using constructor  
+    public Process(int id){  
+        this.id = id;  
+        this.status = "ativo";  
+    }  
+}  
+    Scanner sc;  
+    Process[] processos;  
+    int n;  
+      
+    // initialize Scanner class object in constructor  
+    public AlgoritimoValentao(){  
+        sc= new Scanner(System.in);  
+    }  
+      
+    // create ring() method for initializing the ring  
+    public void ring(){  
+          
+        // get input from the user for processos  
+        System.out.println("Digite o número de processos");  
+        n = sc.nextInt();  
+          
+        // initialize processos array  
+        processos = new Process[n];  
+        for(int i = 0; i< n; i++){  
+            processos[i]= new Process(i);  
+        }  
+    }  
+      
+    // create election() method for electing process  
+    public void performElection(){  
+  
+        // we use the sleep() method to stop the execution of the current thread  
+        try {  
+            Thread.sleep(1000);  
+        } catch (InterruptedException e) {  
+              
+            e.printStackTrace();  
+        }  
+          
+        // show failed process  
+        System.out.println("Processo de id "+processos[getMaxValue()].id+" falhou");  
+          
+        // change status to Inactive of the failed process  
+        processos[getMaxValue()].status = "inativo";  
+          
+        // declare and initialize variables   
+        int idInicio = 0;  
+        boolean overStatus = true;  
+          
+        // use while loop to repeat steps   
+        while(overStatus){  
+              
+            boolean maiorProcesso = false;  
+              
+            // iterate all the processos  
+            for(int i = idInicio + 1; i< n; i++){  
+                if(processos[i].status == "ativo"){  
+                    System.out.println("Processo "+idInicio+" foi eleito("+idInicio+") para o processo " +i);  
+                    maiorProcesso = true;  
+  
+                }  
+            }  
+              
+            // check for higher process  
+            if(maiorProcesso){  
+                  
+                // use for loop to again iterate processos  
+                for(int i = idInicio + 1; i< n; i++){  
+                    if(processos[i].status == "ativo"){  
+                        System.out.println("Processo "+i+" passou Ok("+i+") para o Processo " +idInicio);  
+                    }  
+  
+                }  
+                // increment initiator id   
+                idInicio++;  
+            }  
+  
+            else{  
+                // get the last process from the processos that will become coordinator  
+                int coord = processos[getMaxValue()].id;  
+                  
+                // show process that becomes the coordinator  
+                System.out.println("O processo "+coord+" foi eleito");  
+                  
+                  
+                for(int i = coord - 1; i>= 0; i--){  
+                    if(processos[i].status == "ativo"){  
+                        System.out.println("Processo "+coord+" cedeu a eleição ("+coord+") para o processo " +i);  
+                    }  
+                }  
+                  
+                  
+                System.out.println("Fim da Eleição");  
+                overStatus = false;  
+                break;  
+            }  
+        }  
+  
+    }  
+      
+    // create getMaxValue() method that returns index of max process  
+    public int getMaxValue(){  
+        int mxId = -99;  
+        int mxIdIndex = 0;  
+        for(int i = 0; i<processos.length; i++){  
+            if(processos[i].status == "ativo" && processos[i].id >mxId){  
+                mxId = processos[i].id;  
+                mxIdIndex = i;  
+            }  
+        }  
+        return mxIdIndex;  
+    }  
+      
+    // main() method start  
+    public static void main(String[] args) {  
+          
+        // create instance of the BullyAlgoExample2 class  
+        AlgoritimoValentao bully = new AlgoritimoValentao();  
+          
+        // call ring() and performElection() method  
+        bully.ring();  
+        bully.performElection();  
+  
+    }  
     
 }
